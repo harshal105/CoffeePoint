e7314695b87100eda53d99252600d7841e0db135
Harshal Patel <patelharshal101@gmail.com>
2022-07-08 14:26:18 +0200

    Header Component Added

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+//import logo from "/images/icon.png";
+
+
+
+const Header = () =>{
+    return(
+    <div className="container">
+        <img className= "logo" src="/images/coffee-cup.png" alt="Nopic"/>
+        <h1> CoffeeHub </h1>
+        <span class='line'></span>
+
+    </div>
+    )
+}
+
+export default Header;
