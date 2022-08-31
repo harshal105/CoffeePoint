2e88ec1ea1fbadd448655b155e6940b23ea780af
Harshal Patel <patelharshal101@gmail.com>
2022-08-30 22:23:46 -0600

    Added functionality to show ALL coffees in database

--- client/src/pages/Everything/Everything.jsx
+++ client/src/pages/Everything/Everything.jsx
@@ -1,9 +1,55 @@
+import  Axios  from "axios";
 import React from "react";
 import "./Everything.css";
+import { useState, useEffect } from "react";
+import CoffeeCard from "../../components/CoffeeCard/CoffeeCard";
 
 const Everything = () => {
+    
+    const [allCoffeeInfo, setAllCoffeeInfo] = useState([]);
+    const url = "http://localhost:3001/coffees";
+    
+    useEffect(() => {
+        const getAllCoffees =  async () => {
+           await Axios.get(url)
+            .then(
+                (response) => {
+                    setAllCoffeeInfo(response.data);
+                }
+            )
+        }
+   
+        getAllCoffees();
+
+        console.log(allCoffeeInfo);
+    }, [])
+    
     return(
-        <h1> Everything Page </h1>
+        <div className="everythingDiv">
+            <div className="everythingHeader">
+                <h1> CoffeeHub Gallery </h1>
+                <p className="sentenceBeg"> Below is all of the different coffees we have in our database!</p>
+            </div>
+            {
+                allCoffeeInfo.map( coffee => { 
+                    return(
+                        <CoffeeCard 
+                            key={coffee.id}
+                            name={coffee.name}
+                            country={coffee.country}
+                            date={coffee.date}
+                            picture={coffee.picture}
+                            howToMake={coffee.howToMake}
+                            infoVideo={coffee.infoVideo}
+
+                        />
+                    )
+                })
+            }
+    
+                
+            
+        </div>
     )
 }
 
